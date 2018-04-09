12551ef5a26e55b22995d3db8049328263f6e95e
Scott Goes <littlemansmg@gmail.com>
2018-04-09 16:23:40 -0500

    Update code

--- DMG.py
+++ DMG.py
@@ -16,11 +16,12 @@ Github: https://github.com/Littlemansmg/Discord-Meme-Generator
 from MemeFormatting import *
 from discord.ext import commands
 
-toplist = ['simply']
+toplist = []
+bottomlist = []
 
 topBottomList = [
     '10-guy', 'bad-luck-brian', 'good-guy-greg', 'mocking-spongebob', 'roll-safe',
-    'successkid', 'willy-wonka']
+    'simply', 'successkid', 'willy-wonka']
 
 with open('token.txt') as token:
     token = token.readline()
@@ -36,13 +37,31 @@ async def on_ready():
     # bot.change_presence(game = ")help for help")
 
 @bot.event
-async def on_command_error(ctx, error):
-    destination = error.message.channel
-    if isinstance(ctx, commands.MissingRequiredArgument):
+async def on_command_error(error, ctx):
+    # NOTE: It's stated in the documentation that CTX, should always be first.
+    # for on_command_error, The first paramater is the error, and then the context
+    # so error will always come first.
+    destination = ctx.message.channel
+    if isinstance(error, commands.MissingRequiredArgument):
+        await bot.delete_message(ctx.message)
         await bot.send_message(destination, "You are missing some arguments.")
 
 @bot.command(pass_context=True, name='tb')
 async def topAndBottom(ctx, memeType, topString, bottomString):
+    destination = ctx.message.channel
+    message = ctx.message
+
+    await bot.delete_message(message)
+
+    if memeType in topBottomList:
+        send = top_bottom(memeType, topString, bottomString)
+        await bot.send_file(destination, send)
+
+    else:
+        await bot.send_message(destination, "Sorry @" + str(ctx.message.author) + ", " + memeType + " isn't here.")
+
+@bot.command(pass_context = True, name = 'top')
+async def topText(ctx, memeType, topString):
     destination = ctx.message.channel
     message = ctx.message
 
@@ -53,10 +72,43 @@ async def topAndBottom(ctx, memeType, topString, bottomString):
         await bot.send_file(destination, send)
 
     elif memeType in topBottomList:
-        send = top_bottom(memeType, topString, bottomString)
+        send = top_bottom(memeType, topString, '')
         await bot.send_file(destination, send)
 
-    else:
-        await bot.send_message(destination, "Sorry @" + str(ctx.message.author) + ", " + memeType + " isn't here.")
+@bot.command(pass_context = True, name = 'bottom')
+async def bottomText(ctx, memeType, bottomString):
+    destination = ctx.message.channel
+    message = ctx.message
+
+    await bot.delete_message(message)
+
+    if memeType in toplist:
+        send = top_bottom(memeType, '', bottomString)
+        await bot.send_file(destination, send)
+
+    elif memeType in topBottomList:
+        send = top_bottom(memeType, '', bottomString)
+        await bot.send_file(destination, send)
+
+@bot.command(pass_context = True, name = 'help')
+async def help(ctx):
+    destination = ctx.message.channel
+    message = ctx.message
+
+    listhelp = [toplist, bottomlist, topBottomList]
+    tbhelp = '''\n
+    )tb <memetype> <"top text"> <"bottom text"> - Notes: this command can only be used with memes
+    that are in the Top and Bottom List. Text must be in single or double quotes.\n
+    '''
+    tophelp = '''\n
+    )top <memetype> <"toptext"> - Notes: This command will only print toptext. It can only be used
+    with memes that are in the Top List or Top and Bottom list. Text must be in single or double quotes.\n
+    '''
+    bottomhelp = '''\n
+    )bottom <memetype> <"bottomtext"> - Notes: This command will only print bottomtext. It can only be used
+    with memes that are in the Bottom List or Top and Bottom list. Text must be in single or double quotes.\n
+    '''
+
+    await bot.send_message(destination, (tbhelp, tophelp, bottomhelp))
 
 bot.run(token)
